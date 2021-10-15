1c3c92f46e3e39f774583c325e708ffbac7d13f2
zivserphos <zivfromisrael@gmail.com>
2021-10-15 22:41:45 +0300

    added pop up window with pokemon details

--- index.js
+++ index.js
@@ -28,41 +28,83 @@ async function pokemonsWithType(type) {
     const pokemonWithType = await axios.get(`https://pokeapi.co/api/v2/type/${type}`)
     return pokemonWithType.data.pokemon
 }
-const pokemonData = (pokemon) => [pokemon.types ,pokemon.name , pokemon.weight , pokemon.height , pokemon.sprites.front_default, pokemon.types , pokemon.sprites.back_default]
+const pokemonData = (pokemon) => {
+    const dataPokemon = {
+        name: pokemon.name,
+        weight: pokemon.weight,
+        height: pokemon.height,
+        frontImg: pokemon.sprites.front_default,
+        backImg: pokemon.sprites.back_default,
+        type: pokemon.types,
+    }
+    return dataPokemon
+}
 const pokemons = pokemonsWithType("grass").then((pokemons) => pokemons)
-const pokemon = getPokemon("charmeleon")
 
 async function ninePokemons() {
-    const pokemons = await axios.get(`https://pokeapi.co/api/v2/pokemon/?limit=9&offset=${next}`)
-    for(i=0; i<pokemons.length; i++) {
-        console.log("22")
-        const pokemonData = await getPokemon(pokemons[i].name)
-        console.log(pokemonData)
+    let pokemons = await axios.get(`https://pokeapi.co/api/v2/pokemon/?limit=9&offset=${next}`)
+    pokemons = pokemons.data.results;
+    for(let i=0; i<pokemons.length; i++) {
+        pokemons[i] = await getPokemon(pokemons[i].name)
     }
-    return pokemons.data.results
+    return pokemons
 }
 
 async function presenetPokemons(pokemons) {
-
     for(i=0; i<pokemons.length; i++) {
-        const pokemon = await getPokemon(pokemons[i].name)
-        const img = createElement("img" , [] , [] , {src: `${pokemon[4]}`})
+        const pokemon = pokemons[i]
+        const img = createElement("img" , [] , [] , {src: `${pokemon.frontImg}`})
         const pokEl = document.getElementById(`pok${i+1}`)
         pokEl.textContent = ``
         pokEl.append(img)
     }
-    next+=9;
 }
 
 async function nextPokemons(){
-    console.log("@")
+    next+=9;
     const pokemons = await ninePokemons()
     presenetPokemons(pokemons)
 }
 
+async function previousPokemons() {
+    if (next>8) {
+        next-=9
+    }
+    const pokemons = await ninePokemons()
+    presenetPokemons(pokemons)
+}
+
+function pokemonDetailsEl(pokemon){
+    const name = createElement("li" , [pokemon.name])
+    const weight = createElement("li" , [pokemon.weight])
+    const height = createElement("li" , [pokemon.height])
+    console.log(pokemon.backImg)
+    const img = createElement("img" , [] , ["pokemonImg"] , {src: `${pokemon.frontImg}`, frontImg: pokemon.frontImg ,backImg: pokemon.backImg , onmouseover: "whenHover(event)" , onmouseleave: "whenLeave(event)"})
+    const ul = createElement("ul" , [name,weight,height,img])
+    return ul
+}
+
+function whenHover(event) {
+    const backImg = event.target.getAttribute("backImg")
+    event.target.setAttribute("src" , backImg)
+}
+
+function whenLeave(event) {
+    const frontImg = event.target.getAttribute("frontImg")
+    event.target.setAttribute("src" , frontImg)
+}
+
+async function searchPokemon(event) {
+    const input = document.getElementById("pokemonInput")
+    const pokemon = await getPokemon(input.value)
+    document.querySelector(".modal-body").append(pokemonDetailsEl(pokemon))
+    document.getElementById("showPokemonDetails").style.display = "flex"
+}
+
 ninePokemons().then((pokemons) => presenetPokemons(pokemons))
-//document.getElementById("next").addEventListener("click" , nextPokemons())
-console.log("22")
+document.getElementById("search-addon").addEventListener("click" , (event) => searchPokemon(event))
+
+
 
 
 
