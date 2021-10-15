9bc94e66e6cf82d0aa1d878c60a6fb19981096db
zivserphos <zivfromisrael@gmail.com>
2021-10-15 13:55:10 +0300

    build my html page

--- index.js
+++ index.js
@@ -1,8 +1,21 @@
+function createElement(tagName, children = [], classes = [], attributes = {}) { // create new element in more comfortable
+    const el = document.createElement(tagName); 
+    for (let child of children) { // append childs of element
+        el.append(child)
+    }
+    for (let cls of classes) { // add all the classes to the element
+        el.classList.add(cls)
+    }
+    for (let attr in attributes) { // add all attributes to the element
+        el.setAttribute(attr , attributes[attr])
+    }
+    return el
+}
 
 async function getPokemon(pokemonName) {
     try {
         const x = await axios.get(`https://pokeapi.co/api/v2/pokemon/${pokemonName}/`)
-        return pokemonData(x.data)
+        return x.data
     }
     catch {
         throw new Error("NOT FOUND")
@@ -13,9 +26,15 @@ async function pokemonsWithType(type) {
     const pokemonWithType = await axios.get(`https://pokeapi.co/api/v2/type/${type}`)
     return pokemonWithType.data.pokemon
 }
-
 const pokemonData = (pokemon) => [pokemon.types ,pokemon.name , pokemon.weight , pokemon.height , pokemon.sprites.front_default, pokemon.types , pokemon.sprites.back_default]
-const pokemon = getPokemon("bulbasaur").then((pokemon) => pokemon).catch((err) => console.log(err.message))
 const pokemons = pokemonsWithType("grass").then((pokemons) => pokemons)
+const pokemon = getPokemon("Charmeleon")
 
+
+pokemon.then((pokemon) => {console.log(pokemon)})
+.catch((err) => console.log(err.message)) 
+console.log("fuck")
+
+
+             
 
